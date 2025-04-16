53bff48e15b910b6bddb933ad30d9b574671db55
yeonjae <pyj3740@gmail.com>
2025-04-16 18:07:29 +0900

    ✨ Feat: basic js - sync
    자바스크립트 기초 문법(비동기)

--- basics/02/sync.js
+++ basics/02/sync.js
@@ -1,1 +1,18 @@
 // 순서대로 실행하기  (결과 비교: 02\results\sync.js)
+
+function displayA() {
+  console.log("A");
+}
+function displayB(callback) {
+    // 2초 후에 실행
+    setTimeout(() => {
+        console.log("B");
+        callback();
+    }, 2000);
+}
+function displayC() {
+  console.log("C");
+}
+
+displayA()
+displayB(displayC)
