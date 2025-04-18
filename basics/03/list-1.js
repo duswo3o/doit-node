51e4516f737b971e275f421fc73dfb4349b15f6d
yeonjae <pyj3740@gmail.com>
2025-04-18 15:00:34 +0900

    ✨ Feat: fs module
    파일 경로 출력 및 파일 읽고 쓰기

--- basics/03/list-1.js
+++ basics/03/list-1.js
@@ -1,1 +1,2 @@
 // fs 모듈의 readdirSync 함수 연습하기 ( 결과 비교 파일 : 03\results\list-1.js)
+// readdirSync() - 동기로 처리
--- basics/03/list-2.js
+++ basics/03/list-2.js
@@ -1,1 +1,10 @@
 // fs 모듈의 readdir 함수 연습하기 ( 결과 비교 파일 : 03\results\list-2.js)
+
+const fs = require("fs");
+
+fs.readdir("./", (err, files) => { // 현재 폴더(디렉토리)
+    if (err){
+        console.log(err)    // 에러 발생 시 에러 출력
+    }
+    console.log(files)  // 성공시 파일 출력
+}); 
