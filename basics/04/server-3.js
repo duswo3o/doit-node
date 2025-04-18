ba86df0ba87f96d3bbbe4244424d4363f533b1a1
yeonjae <pyj3740@gmail.com>
2025-04-18 15:46:09 +0900

    ✨ Feat: http module
    http 모듈로 서버 생성 및 실행

--- basics/04/server-3.js
+++ basics/04/server-3.js
@@ -1,1 +1,16 @@
 // 응답 객체 확인하기 - 응답 헤더, 응답 본문, 응답 종료  (결과 비교 파일 : 04\results\server-3.js)
+
+const http = require("http");
+
+// 서버 생성
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-type", "text/plain"); // 사용할 컨텐츠 타입 지정 (일반 순수 텍스트)
+  // 화면에 표시
+  res.write("Hello Node");
+  res.end(); // 응답 종료
+});
+
+// 서버 실행
+server.listen(3000, () => {
+  console.log("서버가 실행중");
+}); // 포트 번호 지정
