1bfd03d3fcf078501beadc54fe4df0972b645efb
yeonjae <pyj3740@gmail.com>
2025-04-18 14:32:37 +0900

    ✨ Feat: path module
    - path 모듈 사용해보기

--- basics/03/path.js
+++ basics/03/path.js
@@ -1,2 +1,16 @@
 // path 모듈 연습하기 ( 결과 비교 파일 : 03\results\path.js)
+const path = require("path");
 
+// join
+const fullPath = path.join("some", "work", "ex.txt");
+console.log(fullPath);
+
+// 경로만 추출 - dirname
+const dir = path.dirname(fullPath); // 파일 이름은 빼고 경로만 추출
+console.log(dir);
+
+// 파일 이름만 추출 - basename
+const fn1 = path.basename(__filename); // 현재 파일의 전체 경로 중 파일 이름만 추출
+console.log(fn1);
+const fn2 = path.basename(__filename, ".js"); // js 확장자 제외하고 파일 이름만 추출
+console.log(fn2);
