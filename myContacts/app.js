c3eeefec79a419f3b080766c7bff96ca4de459ba
yeonjae <pyj3740@gmail.com>
2025-04-22 13:51:38 +0900

    ✨ Feat: express routing
    익스프레스 라우팅

--- myContacts/app.js
+++ myContacts/app.js
@@ -6,8 +6,29 @@ app.get("/", (req, res) => {
   res.send("Hello, Node!");
 });
 
+// 연락처 목록(연락처 가져오기)
 app.get("/concats", (req, res) => {
   res.send("concats Page");
+});
+
+// 새 연락처 추가하기
+app.post("/concats", (req, res) => {
+  res.send("Create Concats");
+});
+
+// 연락처 1개 가져오기
+app.get("/concats/:id", (req, res) => {
+  res.send(`View Concat for ID : ${req.params.id}`);
+});
+
+// 연락처 수정히기
+app.put("/concats/:id", (req, res) => {
+  res.send(`Update Concat for ID : ${req.params.id}`);
+});
+
+// 연락처 삭제하기
+app.delete("/conctas/:id", (req, res) => {
+  res.send(`Delete Concat for ID : ${req.params.id}`);
 });
 
 // app 서버 실행
